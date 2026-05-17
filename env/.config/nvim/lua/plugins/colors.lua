a67a09ecdcf618717f9c7275719bc1e05ed0af33
Flamur38 <mulihtb@proton.me>
2026-05-17 14:43:16 +0200

    updated

--- env/.config/nvim/lua/plugins/colors.lua
+++ env/.config/nvim/lua/plugins/colors.lua
@@ -1,66 +1,14 @@
--- return {
---   "kaiuri/nvim-juliana",
---   lazy = false,
---   priority = 1000,
---   config = function()
---     require("nvim-juliana").setup({
---       transparent = true,
---     })
---     vim.cmd("colorscheme juliana")
---     vim.cmd [[
---       highlight Normal      guibg=NONE ctermbg=NONE
---       highlight NormalNC    guibg=NONE ctermbg=NONE
---       highlight SignColumn  guibg=NONE ctermbg=NONE
---       highlight VertSplit   guibg=NONE ctermbg=NONE
---       highlight EndOfBuffer guibg=NONE ctermbg=NONE
---       highlight CursorLine     guibg=NONE ctermbg=NONE
---       highlight CursorLineNr   guibg=NONE ctermbg=NONE
---
---       highlight StatusLine   guibg=NONE ctermbg=NONE
---       highlight StatusLineNC guibg=NONE ctermbg=NONE
---     ]]
---   end,
--- }
 
--- return {
---   "rose-pine/neovim",
---   name = "rose-pine",
---   lazy = false,
---   priority = 1000,
---   config = function()
---     require("rose-pine").setup({
---       variant = "main",
---       transparent = true,
---       terminal_colors = true,
---       styles = {
---         bold = true,
---         italic = true,
---         transparency = true,
---       },
---     })
---     vim.cmd("colorscheme rose-pine")
---     vim.cmd [[
---       highlight Normal      guibg=NONE ctermbg=NONE
---       highlight NormalNC    guibg=NONE ctermbg=NONE
---       highlight SignColumn  guibg=NONE ctermbg=NONE
---       highlight VertSplit   guibg=NONE ctermbg=NONE
---       highlight EndOfBuffer guibg=NONE ctermbg=NONE
---       highlight CursorLine     guibg=NONE ctermbg=NONE
---       highlight CursorLineNr   guibg=NONE ctermbg=NONE
---       highlight StatusLine   guibg=NONE ctermbg=NONE
---       highlight StatusLineNC guibg=NONE ctermbg=NONE
---     ]]
---   end,
--- }
-
+-- my colorscheme
 return {
   {
     dir = vim.fn.stdpath("config"),
-    name = "ubuntu",
+    name = "fl4my_cold",
     lazy = false,
     priority = 1000,
     config = function()
-      vim.cmd("colorscheme ubuntu")
+      vim.cmd("colorscheme fl4my_cold")
     end,
   }
 }
+

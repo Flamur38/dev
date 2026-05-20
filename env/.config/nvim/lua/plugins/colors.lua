844a0a27973438ca8fa15d3a2e5976c65ca3c1d8
Flamur38 <mulihtb@proton.me>
2026-05-20 15:57:22 +0200

    updated

--- env/.config/nvim/lua/plugins/colors.lua
+++ env/.config/nvim/lua/plugins/colors.lua
@@ -1,14 +1,34 @@
 
 -- my colorscheme
 return {
-  {
-    dir = vim.fn.stdpath("config"),
-    name = "green",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      vim.cmd("colorscheme green")
-    end,
-  }
-}
+    -- {
+        --   dir = vim.fn.stdpath("config"),
+        --   name = "green",
+        --   lazy = false,
+        --   priority = 1000,
+        --   config = function()
+            --     vim.cmd("colorscheme green")
+            --   end,
+            -- }
+            {
+                "rose-pine/neovim",
+                name = "rose-pine",
+                lazy = false,
+                priority = 1000,
+                config = function()
+                    require("rose-pine").setup({
+                        disable_background = true,
+                        styles = {
+                            italic = false,
+                        },
+                        highlight_groups = {
+                            CursorLine = { bg = "NONE" },
+                            StatusLine = { bg = "NONE" },
+                        },
+
+                    })
+                    vim.cmd("colorscheme rose-pine")
+                end,
+            },
+        }
 

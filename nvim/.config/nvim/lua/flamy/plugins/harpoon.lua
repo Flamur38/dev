fc1c63aecba377bc11c0e80b15ec50c6507f9099
Flamur38 <mulihtb@proton.me>
2026-07-02 17:05:18 +0200

    updated

--- nvim/.config/nvim/lua/flamy/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/flamy/plugins/harpoon.lua
@@ -41,15 +41,15 @@ return {
 
             -- Jump directly to harpooned file by index
             -- These are your 4 fast-access slots
-            vim.keymap.set("n", "<leader>1", function()
+            vim.keymap.set("n", "<C-j>", function()
                 harpoon:list():select(1)
             end, { desc = "Harpoon: file 1" })
 
-            vim.keymap.set("n", "<leader>2", function()
+            vim.keymap.set("n", "<C-k>", function()
                 harpoon:list():select(2)
             end, { desc = "Harpoon: file 2" })
 
-            vim.keymap.set("n", "<leader>3", function()
+            vim.keymap.set("n", "<C-l>", function()
                 harpoon:list():select(3)
             end, { desc = "Harpoon: file 3" })
 
